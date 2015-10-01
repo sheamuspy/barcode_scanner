ce7e5260129bd48809d5983732de5f6146cb9fa8
sheamuspy <sheamuspy@gmail.com>
2015-10-01 02:55:00 +0000

    minor bug fixes

--- js/barcode_scanner.js
+++ js/barcode_scanner.js
@@ -4,13 +4,15 @@ $("#scan").click(function () {
     "use strict";
     cordova.plugins.barcodeScanner.scan(
         function (result) {
-            alert("We got a barcode\n" +
-                    "Result: " + result.text + "\n" +
-                    "Format: " + result.format + "\n" +
-                    "Cancelled: " + result.cancelled);
+//            alert("We got a barcode\n" +
+//                    "Result: " + result.text + "\n" +
+//                    "Format: " + result.format + "\n" +
+//                    "Cancelled: " + result.cancelled);
+            $("#result").val(result.text);
         },
         function (error) {
-            alert("Scanning failed: " + error);
+//            alert("Scanning failed: " + error);
+            $("#result").val(error);
         }
     );
 });
