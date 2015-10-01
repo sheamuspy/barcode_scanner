c52d67ba2c44842abcb61304317dbb210adcc3c3
sheamuspy <sheamuspy@gmail.com>
2015-10-01 03:21:08 +0000

    modified js to load function when document is ready

--- js/barcode_scanner.js
+++ js/barcode_scanner.js
@@ -1,18 +1,20 @@
 /*jslint browser: true*/
 /*global  $, jQuery, alert, cordova*/
-$("#scan").click(function () {
+$(function () {
     "use strict";
-    cordova.plugins.barcodeScanner.scan(
-        function (result) {
-//            alert("We got a barcode\n" +
-//                    "Result: " + result.text + "\n" +
-//                    "Format: " + result.format + "\n" +
-//                    "Cancelled: " + result.cancelled);
-            $("#result").val(result.text);
-        },
-        function (error) {
-//            alert("Scanning failed: " + error);
-            $("#result").val(error);
-        }
-    );
+    $("#scan").click(function () {
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+    //            alert("We got a barcode\n" +
+    //                    "Result: " + result.text + "\n" +
+    //                    "Format: " + result.format + "\n" +
+    //                    "Cancelled: " + result.cancelled);
+                $("#result").val(result.text);
+            },
+            function (error) {
+    //            alert("Scanning failed: " + error);
+                $("#result").val(error);
+            }
+        );
+    });
 });
